34356bb1d5021101502f8d0008b4256b1fb7a249
curt vance <c.vance7777@hotmail.com>
2026-02-05 18:57:46 -0500

    update

--- index.js
+++ index.js
@@ -4,6 +4,23 @@
 
 
 function contact(event) {
-    event.preventDefualt();
-    console.log("it worked")
+   event.preventDefault();
+   const loading = document.querySelector('.modal__overlay--loading');
+   const success = document.querySelector('.modal__overlay--success');
+   loading.classList += " modal__overlay--visible" 
+   emailjs
+    .sendForm(
+    'service_qrp0o71',
+    'template_e0nx6hd',
+    event.target,
+    'sf8KrxlWlDkrSqJDS'
+    ).then(() => {
+    loading.classList.remove("modal__overlay--visible");
+    success.classList += " modal__overlay--visible";
+    }).catch(() => {
+    loading.classList.remove("modal__overlay--visible");
+    alert(
+    "The email service is temporarily unavalible. Please contact me directly on c.vance7777@hotmail.com"
+    );
+    })
 }
